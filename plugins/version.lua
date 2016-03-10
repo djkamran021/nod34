a2df9baceedba09db005321f490a9f6d2c28e741
mehdi <mehdizare863@yahoo.com>
2016-03-10 17:11:48 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,15 +1,15 @@
 do
 
 function run(msg, matches)
-  return 'NOD32-BOT v8'.. VERSION .. [[ 
+  return 'kingbot v8'.. VERSION .. [[ 
  
-  Developer : @Behroozyaghi
+  Developer : @mehdijokers
   
-  Channel : @Nod32team
+  Channel : @kingbotpluss
   
-  Open git : https://github.com/BH-YAGHI/NOD32-BOT.git
+  Open git : https://github.com
   
-  NOD32 VERSION 8
+  kingbot VERSION 8
   
   All rights reserved.
   __________________]]
